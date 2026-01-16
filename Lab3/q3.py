085fc5a63f0ff0ae27f354aad42282958e837909
Suhani Bansal <rs.suhanibansal@gmail.com>
2026-01-16 22:44:56 +0530

    update

--- Lab3/q3.py
+++
@@ -1,8 +0,0 @@
-
-def load_data(datafile):
-    file = open(datafile, "r")
-    content = file.read()
-
-def div_sets(content):
-    x = content.drop(columns=["disease_score_fluct"])
-    y = content["disease_score_fluct"]
